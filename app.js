fc9881d7d0782eccc3338f3ed8b22b0ef8d7c116
sowmya-yalamarthi <s542049@nwmissouri.edu>
2021-03-17 14:45:38 -0500

    changes made

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const server =
     (req,res)=> {               //Anonymous function to handle the request
       res.statusCode = 200      //code for OK
       res.setHeader('Content-Type', 'text/plain') //Set the mime type
-      res.end('Hello World')    //Close the response and provide content
+      res.end('I love travelling and explore things out there ,even a small place leaves many memories')    //Close the response and provide content
     }                           //No return needed, we modified the res object we got
 )
 
